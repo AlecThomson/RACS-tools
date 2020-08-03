c1f4c39ae0ebf29efb4b6c7a898a944ac2dabb26
Thomson, Alec (CASS, Kensington) <Alec.Thomson@csiro.au>
2020-08-03 16:25:20 +0800

    Using MPI

--- beamcon_3D.py
+++ beamcon_3D.py
@@ -5,6 +5,7 @@ from logging import disable
 from operator import countOf
 from spectral_cube.utils import SpectralCubeWarning
 import warnings
+from astropy.utils.exceptions import AstropyWarning
 import os
 import stat
 import sys
@@ -16,19 +17,23 @@ from astropy.table import Table
 from spectral_cube import SpectralCube
 from radio_beam import Beam, Beams
 from radio_beam.utils import BeamError
-import schwimmbad
 from tqdm import tqdm, trange
 import au2
 import functools
 import psutil
 from IPython import embed
-try:
-    print = functools.partial(
-        print, f'[{psutil.Process().cpu_num()}]', flush=True)
-except AttributeError:
-    print = functools.partial(print, flush=True)
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+#n_cores = comm.Get_size()
+print = functools.partial(print, f'[{comm.rank}]', flush=True)
+#try:
+#    print = functools.partial(
+#        print, f'[{psutil.Process().cpu_num()}]', flush=True)
+#except AttributeError:
+#    print = functools.partial(print, flush=True)
 warnings.filterwarnings(action='ignore', category=SpectralCubeWarning,
                         append=True)
+warnings.simplefilter('ignore', category=AstropyWarning)
 
 #############################################
 #### ADAPTED FROM SCRIPT BY T. VERNSTROM ####
@@ -222,20 +227,21 @@ def cpu_to_use(max_cpu, count):
     return max(factors[factors <= max_cpu])
 
 
-def worker(idx, start, cubedict):
+def worker(idx, cubedict, start=0):
     cube = SpectralCube.read(cubedict["filename"])
     plane = cube.unmasked_data[start+idx].value
-    newim = smooth(plane, cubedict['dy'], cubedict['conbeams']
-                   [start+idx], cubedict['sfactors'][start+idx], verbose=False)
+    newim = smooth(plane, cubedict['dy'], cubedict['convbeams']
+                   [start+idx], cubedict['facs'][start+idx], verbose=False)
     return newim
 
 
-def makedata(files):
+def makedata(files, outdir):
     datadict = {}
-    for i, file in enumerate(files):
+    for i, (file, out) in enumerate(zip(files, outdir)):
         # Set up files
         datadict[f"cube_{i}"] = {}
         datadict[f"cube_{i}"]["filename"] = file
+        datadict[f"cube_{i}"]["outdir"] = out
         # Get metadata
         header = fits.getheader(file)
         dxas = header['CDELT1']*-1*u.deg
@@ -360,6 +366,10 @@ def commonbeamer(datadict, nchans, args, mode='natural', verbose=True):
         pas *= pa.unit
         big_beams = Beams(major=majors, minor=minors, pa=pas)
 
+        if verbose:
+            print('Finding common beam across all channels')
+            print('This may take some time...')
+
         try:
             commonbeam = big_beams[~np.isnan(big_beams)].common_beam(tolerance=args.tolerance,
                                                                      nsamps=args.nsamps,
@@ -393,7 +403,7 @@ def commonbeamer(datadict, nchans, args, mode='natural', verbose=True):
         print('Final beams are:')
         for i, commonbeam in enumerate(commonbeams):
             print(f'Channel {i}:', commonbeam)
-    embed()
+
     for key in tqdm(
         datadict.keys(),
         desc='Getting convolution data',
@@ -437,6 +447,7 @@ def commonbeamer(datadict, nchans, args, mode='natural', verbose=True):
         datadict[key]['convbeams'] = convbeams
         commonbeam_log = datadict[key]['beamlog'].replace('beamlog.',
                                                           f'beamlogConvolve-{mode}.')
+        datadict[key]['commonbeams'] = commonbeams
         datadict[key]['commonbeamlog'] = commonbeam_log
 
         commonbeam_tab = Table()
@@ -489,162 +500,246 @@ def masking(nchans, cutoff, datadict, verbose=True):
     return datadict
 
 
-def main(pool, args, verbose=True):
+def initfiles(datadict, nchans, mode, verbose=True):
+    for key in tqdm(datadict.keys(), desc='Initialising cubes'):
+        with fits.open(datadict[key]["filename"], memmap=True, mode='denywrite') as hdulist:
+            primary_hdu = hdulist[0]
+            data = primary_hdu.data
+            header = primary_hdu.header
+
+        # Header
+        commonbeams = datadict[key]['commonbeams']
+        header = commonbeams[0].attach_to_header(header)
+        primary_hdu = fits.PrimaryHDU(data=data, header=header)
+        if mode == 'natural':
+            header['COMMENT'] = 'The PSF in each image plane varies.'
+            header['COMMENT'] = 'Full beam information is stored in the second FITS extension.'
+            beam_table = Table(
+                data=[
+                    commonbeams.major.to(u.arcsec),
+                    commonbeams.minor.to(u.arcsec),
+                    commonbeams.pa.to(u.deg)
+                ],
+                names=[
+                    'BMAJ',
+                    'BMIN',
+                    'BPA'
+                ]
+            )
+            
+            tab_hdu = fits.table_to_hdu(beam_table)
+            new_hdulist = fits.HDUList([primary_hdu, tab_hdu])
+
+        elif mode == 'total':
+            new_hdulist = fits.HDUList([primary_hdu])
+        # Set up output file
+        outname = f"sm-{mode}." + os.path.basename(datadict[key]["filename"])
+        outdir = datadict[key]['outdir']
+        outfile = f'{outdir}/{outname}'
+        datadict[key]['outfile'] = outfile
+        if verbose:
+            print(f'Initialsing to {outfile}')
+
+        new_hdulist.writeto(outfile, overwrite=True)
+    
+    return datadict
+
+    '''
+    if not args.mpi:
+        n_cores = args.n_cores
+    width_max = n_cores
+    width = cpu_to_use(width_max, cube.shape[0])
+    n_chunks = cube.shape[0]//width
+
+    for i in trange(
+            n_chunks, disable=(not verbose),
+            desc='Smoothing in chunks'
+    ):
+        start = i*width
+        stop = start+width
+
+        func = functools.partial(
+            worker, start=start, cubedict=cubedict)
+        arr_out = list(pool.map(func, [idx for idx in range(width)]))
+        arr_out = np.array(arr_out)
+
+        with fits.open(outfile, mode='update', memmap=True) as outfh:
+            outfh[0].data[start:stop, 0, :, :] = arr_out[:]
+            outfh.flush()
+
+    if verbose:
+        print('Updating header...')
+    with fits.open(outfile, mode='update', memmap=True) as outfh:
+        outfh[0].header = new_beam.attach_to_header(outfh[0].header)
+        outfh.flush()
+    # print(arr_out)
+    '''
+
+
+def main(args, verbose=True):
     """main script
     """
-    # Parse args
-    if args.dryrun:
+    #comm = MPI.COMM_WORLD
+    nPE = comm.Get_size()
+    myPE = comm.Get_rank()
+
+    if comm.Get_rank() == 0:
+        print(f"Total number of MPI ranks = {nPE}")
+        # Parse args
+        if args.dryrun:
+            if verbose:
+                print('Doing a dry run -- no files will be saved')
+
+        # Check mode
+        mode = args.mode
         if verbose:
-            print('Doing a dry run -- no files will be saved')
+            print(f"Mode is {mode}")
+        if mode == 'natural' and mode == 'total':
+            raise Exception("'mode' must be 'natural' or 'total'")
+        if mode == 'natural':
+            if verbose:
+                print('Smoothing each channel to a common resolution')
+        if mode == 'total':
+            if verbose:
+                print('Smoothing all channels to a common resolution')
 
-    # Check mode
-    mode = args.mode
-    if verbose:
-        print(f"Mode is {mode}")
-    if mode == 'natural' and mode == 'total':
-        raise Exception("'mode' must be 'natural' or 'total'")
-    if mode == 'natural':
-        if verbose:
-            print('Smoothing each channel to a common resolution')
-    if mode == 'total':
-        if verbose:
-            print('Smoothing all channels to a common resolution')
+        # Check cutoff
+        cutoff = args.cutoff
+        if args.cutoff is not None:
+            cutoff = args.cutoff * u.arcsec
+            if verbose:
+                print('Cutoff is:', cutoff)
 
-    # Check cutoff
-    if args.cutoff is not None:
-        cutoff = args.cutoff * u.arcsec
-        if verbose:
-            print('Cutoff is:', cutoff)
+        # Check target
+        bmaj = args.bmaj
+        bmin = args.bmin
+        bpa = args.bpa
 
-    # Check target
-    bmaj = args.bmaj
-    bmin = args.bmin
-    bpa = args.bpa
+        nonetest = [test is None for test in [bmaj, bmin, bpa]]
 
-    nonetest = [test is None for test in [bmaj, bmin, bpa]]
+        if not all(nonetest) and mode is not 'total':
+            raise Exception("Only specify a target beam in 'total' mode")
 
-    if not all(nonetest) and mode is not 'total':
-        raise Exception("Only specify a target beam in 'total' mode")
+        if all(nonetest):
+            target_beam = None
 
-    if all(nonetest):
-        target_beam = None
+        elif not all(nonetest) and any(nonetest):
+            raise Exception('Please specify all target beam params!')
 
-    elif not all(nonetest) and any(nonetest):
-        raise Exception('Please specify all target beam params!')
+        elif not all(nonetest) and not any(nonetest):
+            target_beam = Beam(
+                bmaj * u.arcsec,
+                bmin * u.arcsec,
+                bpa * u.deg
+            )
+            if verbose:
+                print('Target beam is ', target_beam)
 
-    elif not all(nonetest) and not any(nonetest):
-        target_beam = Beam(
-            bmaj * u.arcsec,
-            bmin * u.arcsec,
-            bpa * u.deg
+        files = sorted(args.infile)
+        if files == []:
+            raise Exception('No files found!')
+        
+        outdir = args.outdir
+        if outdir is not None:
+            if outdir[-1] == '/':
+                outdir = outdir[:-1]
+            outdir = [outdir] * len(files)
+        else:
+            outdir = [os.path.dirname(f) for f in files]
+
+        datadict = makedata(files, outdir)
+
+        # Sanity check channel counts
+        nchans = np.array([datadict[key]['nchan'] for key in datadict.keys()])
+        check = all(nchans == nchans[0])
+
+        if not check:
+            raise Exception('Unequal number of spectral channels!')
+
+        else:
+            nchans = nchans[0]
+
+        # Construct Beams objects
+        for key in datadict.keys():
+            beam = datadict[key]['beam']
+            bmaj = np.array(beam['BMAJ'])*beam['BMAJ'].unit
+            bmin = np.array(beam['BMIN'])*beam['BMIN'].unit
+            bpa = np.array(beam['BPA'])*beam['BPA'].unit
+            beams = Beams(
+                major=bmaj,
+                minor=bmin,
+                pa=bpa
+            )
+            datadict[key]['beams'] = beams
+
+        # Apply some masking
+        datadict = masking(
+            nchans,
+            cutoff,
+            datadict,
+            verbose=verbose
         )
-        if verbose:
-            print('Target beam is ', target_beam)
 
-    # Fix up outdir
-    if args.mpi:
-        from mpi4py import MPI
-        mpiComm = MPI.COMM_WORLD
-        n_cores = mpiComm.Get_size()
-        #mpiRank = mpiComm.Get_rank()
-    outdir = args.outdir
-    if outdir is not None:
-        if outdir[-1] == '/':
-            outdir = outdir[:-1]
-    else:
-        outdir = '.'
-
-    files = sorted(args.infile)
-    if files == []:
-        raise Exception('No files found!')
-
-    datadict = makedata(files)
-
-    # Sanity check channel counts
-    nchans = np.array([datadict[key]['nchan'] for key in datadict.keys()])
-    check = all(nchans == nchans[0])
-
-    if not check:
-        raise Exception('Unequal number of spectral channels!')
-
-    else:
-        nchans = nchans[0]
-
-    # Construct Beams objects
-    for key in datadict.keys():
-        beam = datadict[key]['beam']
-        bmaj = np.array(beam['BMAJ'])*beam['BMAJ'].unit
-        bmin = np.array(beam['BMIN'])*beam['BMIN'].unit
-        bpa = np.array(beam['BPA'])*beam['BPA'].unit
-        beams = Beams(
-            major=bmaj,
-            minor=bmin,
-            pa=bpa
+        datadict = commonbeamer(
+            datadict,
+            nchans,
+            args,
+            mode=mode,
+            verbose=verbose
         )
-        datadict[key]['beams'] = beams
 
-    # Apply some masking
-    datadict = masking(
-        nchans,
-        cutoff,
-        datadict,
-        verbose=verbose
-    )
+        if not args.dryrun:
+            datadict = initfiles(datadict, nchans, mode, verbose=verbose)
+            
+            inputs = []
+            for key in datadict.keys():
+                for chan in range(nchans):
+                    inputs.append( (key,chan) )
+        
 
-    datadict = commonbeamer(
-        datadict,
-        nchans,
-        args,
-        mode=mode,
-        verbose=verbose
-    )
-
+    else:
+        if not args.dryrun:
+            files = None
+            datadict = None
+            nchans = None
+            inputs = None
+    
+    comm.Barrier()
     if not args.dryrun:
-        for key in tqdm(datadict.keys(), desc='Working on cubes separately'):
-            conbms, facs = getfacs(datadict[key], new_beam, verbose=False)
-            cube = SpectralCube.read(datadict[key]["filename"])
-            # Set up output file
-            outname = "sm." + os.path.basename(datadict[key]["filename"])
-            outfile = f'{outdir}/{outname}'
-            if verbose:
-                print(f'Initialsing to {outfile}')
-            if not os.path.isfile(outfile):
-                copyfile(datadict[key]["filename"], outfile, verbose=True)
+        files = comm.bcast(files, root=0)
+        datadict = comm.bcast(datadict, root=0)
+        nchans = comm.bcast(nchans, root=0)
+        inputs = comm.bcast(inputs, root=0)
 
-            cubedict = datadict[key]
-            cubedict["conbeams"] = conbms
-            cubedict["sfactors"] = facs
+        dims = len(files) * nchans
+        assert len(inputs) == dims
+        count = dims // nPE
+        rem = dims % nPE
+        if myPE < rem:
+            # The first 'remainder' ranks get 'count + 1' tasks each
+            my_start = myPE * (count + 1)
+            my_end = my_start + count
 
-            if not args.mpi:
-                n_cores = args.n_cores
-            width_max = n_cores
-            width = cpu_to_use(width_max, cube.shape[0])
-            n_chunks = cube.shape[0]//width
+        else:
+            #The remaining 'size - remainder' ranks get 'count' task each
+            my_start = myPE * count + rem
+            my_end = my_start + (count - 1)
 
-            for i in trange(
-                    n_chunks, disable=(not verbose),
-                    desc='Smoothing in chunks'
-            ):
-                start = i*width
-                stop = start+width
-
-                func = functools.partial(
-                    worker, start=start, cubedict=cubedict)
-                arr_out = list(pool.map(func, [idx for idx in range(width)]))
-                arr_out = np.array(arr_out)
-
-                with fits.open(outfile, mode='update', memmap=True) as outfh:
-                    outfh[0].data[start:stop, 0, :, :] = arr_out[:]
-                    outfh.flush()
-
-            if verbose:
-                print('Updating header...')
+        print(f"My start is {my_start}")
+        print(f"My end is {my_end}")
+        print(f"There are {nchans} channels, across {len(files)} files")
+        for inp in inputs[my_start:my_end+1]:
+            key, chan = inp
+            print(key, chan)
+            newim = worker(chan, datadict[key])
+            outfile = datadict[key]['outfile']
             with fits.open(outfile, mode='update', memmap=True) as outfh:
-                outfh[0].header = new_beam.attach_to_header(outfh[0].header)
+                outfh[0].data[chan, 0, :, :] = newim
                 outfh.flush()
-            # print(arr_out)
-    if verbose:
-        print('Done!')
+            print(f"{outfile}  - channel {chan} - Done")
+    else:
+        if verbose:
+            print('Done!')
 
 
 def cli():
@@ -706,7 +801,7 @@ def cli():
         dest='outdir',
         type=str,
         default=None,
-        help='Output directory of smoothed FITS image(s) [./].'
+        help='Output directory of smoothed FITS image(s) [None - same as input].'
     )
 
     parser.add_argument(
@@ -778,36 +873,11 @@ def cli():
         help="nsamps for radio_beam.commonbeam."
     )
 
-    group = parser.add_mutually_exclusive_group()
-
-    group.add_argument(
-        "--ncores",
-        dest="n_cores",
-        default=1,
-        type=int,
-        help="Number of processes (uses multiprocessing)."
-    )
-    group.add_argument(
-        "--mpi",
-        dest="mpi",
-        default=False,
-        action="store_true",
-        help="Run with MPI."
-    )
-
     args = parser.parse_args()
 
     verbose = args.verbose
 
-    pool = schwimmbad.choose_pool(
-        mpi=args.mpi, processes=args.n_cores)
-    if args.mpi:
-        if not pool.is_master():
-            pool.wait()
-            sys.exit(0)
-
-    main(pool, args, verbose=verbose)
-    pool.close()
+    main(args, verbose=verbose)
 
 
 if __name__ == "__main__":
