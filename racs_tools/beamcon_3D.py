380100a5ce4ae4487da76d84427c54596f987ee4
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2021-09-03 09:47:36 +0800

    debugs

--- racs_tools/beamcon_3D.py
+++ racs_tools/beamcon_3D.py
@@ -221,7 +221,9 @@ def smooth(image, dx, dy, oldbeam, newbeam, conbeam, sfactor, conv_mode="robust"
         return image
     else:
         # using Beams package
+        log.debug(f"Old beam is {oldbeam!r}")
         log.debug(f"Using convolving beam {conbeam!r}")
+        log.debug(f"Target beam is {newbeam!r}")
         log.debug(f"Using scaling factor {sfactor}")
         pix_scale = dy
         gauss_kern = conbeam.as_kernel(pix_scale)
