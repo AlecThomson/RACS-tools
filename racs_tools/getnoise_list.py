44c77bc4db853c93f61b4d45c724167c419959e9
Thomson, Alec (S&A, Kensington) <AlecThomson@users.noreply.github.com>
2022-12-07 12:41:48 +0800

    Refactor

--- racs_tools/getnoise_list.py
+++ racs_tools/getnoise_list.py
@@ -24,35 +24,6 @@ print = functools.partial(print, f"[{psutil.Process().cpu_num()}]", flush=True)
 #############################################
 ####### ADAPTED FROM SCRIPT BY G. HEALD #####
 #############################################
-def calcnoise(args: Tuple[int, str, Union[np.ndarray, None], bool]) -> u.Quantity:
-    """Get noise in plane from cube."""
-    i, file, totalbad, update = args
-    cube = getcube(file)
-    plane = cube.unmasked_data[i]
-    unit = plane.unit
-    if update:
-        print(f"Checking channel {i}")
-    if totalbad is not None and totalbad[i]:
-        return np.nan * unit
-    imsize = plane.shape
-    assert len(imsize) == 2
-    nx = imsize[-1]
-    ny = imsize[-2]
-    Id = plane[ny // 3 : 2 * ny // 3, nx // 3 : 2 * nx // 3].flatten()
-    if len(Id[np.isnan(Id)]) == len(Id):
-        return -1.0 * unit
-    rms = np.std(Id)
-    mval = np.mean(Id)
-    Id = Id[np.logical_and(Id < mval + 3.0 * rms, Id > mval - 3.0 * rms)]
-    Ih = np.histogram(Id, bins=100)
-    if max(Ih[0]) == 0.0:
-        return -1.0
-    Ix = Ih[1][:-1] + 0.5 * (Ih[1][1] - Ih[1][0])
-    Iv = Ih[0] / float(max(Ih[0]))
-    Inoise = myfit(Ix, Iv)
-    return Inoise
-
-
 def getcube(filename: str) -> SpectralCube:
     """Read FITS file as SpectralCube
 
@@ -66,85 +37,46 @@ def getcube(filename: str) -> SpectralCube:
 
 
 def getbadchans(
-    pool: Union[schwimmbad.SerialPool, schwimmbad.MultiPool, schwimmbad.MPIPool],
     qcube: SpectralCube,
     ucube: SpectralCube,
-    ufile: str,
-    qfile: str,
-    totalbad: Union[np.ndarray, None] = None,
     cliplev: float = 5,
-    update: bool = False,
+    ncores: int = 1,
 ) -> np.ndarray:
     """Find deviated channels"""
     assert len(ucube.spectral_axis) == len(qcube.spectral_axis)
-    inputs = [[i, qfile, totalbad, update] for i in range(len(qcube.spectral_axis))]
-    if isinstance(pool, schwimmbad.SerialPool) or isinstance(pool, schwimmbad.MPIPool):
-        print(f"Checking Q...")
-        tic = time.perf_counter()
-        qnoisevals_list = list(pool.map(calcnoise, inputs))
-        toc = time.perf_counter()
-        print(f"Time taken was {toc - tic}s")
 
-    elif pool.__class__.__name__ == "MultiPool":
-        qnoisevals_list = list(
-            tqdm(
-                pool.imap_unordered(calcnoise, inputs),
-                total=len(ucube.spectral_axis),
-                desc="Checking Q",
-            )
-        )
-    qnoisevals = np.array([q.value for q in qnoisevals_list]) * qnoisevals_list[0].unit
+    qnoisevals = qcube.apply_function_parallel_spectral(
+        function=mad_std,
+        num_cores=ncores,
+        parallel=True,
+        use_memmap=True,
+        kwargs=dict(ignore_nan=True),
+    )
+    unoisevals = ucube.apply_function_parallel_spectral(
+        function=mad_std,
+        num_cores=ncores,
+        parallel=True,
+        use_memmap=True,
+        kwargs=dict(ignore_nan=True),
+    )
+    qmeannoise = np.nanmedian(qnoisevals)
+    qstdnoise = mad_std(qnoisevals, ignore_nan=True)
+    print(f"Q noise={qmeannoise:0.3f}±{qstdnoise:0.3f}")
+    umeannoise = np.nanmedian(unoisevals)
+    ustdnoise = mad_std(unoisevals, ignore_nan=True)
+    print(f"U noise={umeannoise:0.3f}±{ustdnoise:0.3f}")
 
-    inputs = [[i, ufile, totalbad, update] for i in range(len(ucube.spectral_axis))]
-    if pool.__class__.__name__ == "MPIPool" or pool.__class__.__name__ == "SerialPool":
-        print(f"Checking U...")
-        tic = time.perf_counter()
-        unoisevals_list = list(pool.map(calcnoise, inputs))
-        toc = time.perf_counter()
-        print(f"Time taken was {toc - tic}s")
-
-    elif pool.__class__.__name__ == "MultiPool":
-        unoisevals_list = list(
-            tqdm(
-                pool.imap_unordered(calcnoise, inputs),
-                total=len(ucube.spectral_axis),
-                desc="Checking U",
-            )
-        )
-    unoisevals = np.array([u.valye for u in unoisevals_list]) * unoisevals_list[0].unit
-
-    qmeannoise = np.median(qnoisevals[abs(qnoisevals) < 1.0])
-    qstdnoise = np.std(qnoisevals[abs(qnoisevals) < 1.0])
-    print("Q median, std:", qmeannoise, qstdnoise)
-    umeannoise = np.median(unoisevals[abs(unoisevals) < 1.0])
-    ustdnoise = np.std(unoisevals[abs(unoisevals) < 1.0])
-    print("U median, std:", umeannoise, ustdnoise)
     qbadones = np.logical_or(
-        qnoisevals > (qmeannoise + cliplev * qstdnoise), qnoisevals == -1.0
+        qnoisevals > (qmeannoise + cliplev * qstdnoise), ~np.isfinite(qnoisevals)
     )
     ubadones = np.logical_or(
-        unoisevals > (umeannoise + cliplev * ustdnoise), unoisevals == -1.0
+        unoisevals > (umeannoise + cliplev * ustdnoise), ~np.isfinite(unoisevals)
     )
-    print(
-        sum(np.asarray(qbadones, dtype=int)),
-        "of",
-        len(qcube.spectral_axis),
-        "are bad (Q)",
-    )
-    print(
-        sum(np.asarray(ubadones, dtype=int)),
-        "of",
-        len(ucube.spectral_axis),
-        "are bad (U)",
-    )
-    new_totalbad = np.logical_or(qbadones, ubadones)
-    print(
-        np.sum(np.asarray(new_totalbad, dtype=int)),
-        "of",
-        len(qcube.spectral_axis),
-        "are bad in Q -or- U",
-    )
-    return new_totalbad
+    print(f"{qbadones.sum()} of {len(qcube.spectral_axis)} are bad in Q")
+    print(f"{ubadones.sum()} of {len(ucube.spectral_axis)} are bad in U")
+    total_bad = np.logical_or(qbadones, ubadones)
+    print(f"{total_bad.sum()} of {len(qcube.spectral_axis)} are bad in Q -or- U")
+    return total_bad
 
 
 def blankchans(
@@ -160,71 +92,68 @@ def blankchans(
         )
     print(f"Bad channels are {badchans}")
     print(f"Bad frequencies are {badfreqs}")
-    totalgood = [not bad for bad in totalbad]
+    totalgood = ~totalbad
     q_msk = qcube.mask_channels(totalgood)
     u_msk = ucube.mask_channels(totalgood)
     return q_msk, u_msk
 
 
-def writefits(qcube: SpectralCube, ucube: SpectralCube, clargs: argparse.Namespace) -> None:
+def writefits(qcube: SpectralCube, ucube: SpectralCube, qfile: str, ufile: str) -> None:
     """Write output to disk"""
-    outfile = clargs.qfitslist.replace(".fits", ".blanked.fits")
+    outfile = qfile.replace(".fits", ".blanked.fits")
     print(f"Writing to {outfile}")
     qcube.write(outfile, format="fits", overwrite=True)
-    outfile = clargs.ufitslist.replace(".fits", ".blanked.fits")
+    outfile = ufile.replace(".fits", ".blanked.fits")
     print(f"Writing to {outfile}")
     ucube.write(outfile, format="fits", overwrite=True)
 
 
-def main(pool, clargs):
-    qcube = getcube(clargs.qfitslist)
-    ucube = getcube(clargs.ufitslist)
+def main(
+    qfile: str,
+    ufile: str,
+    blank: bool = False,
+    cliplev: float = 5,
+    iterate: int = 1,
+    ncores: int = 1,
+    outfile: str = None,
+) -> None:
+    """Flag bad channels in Stokes Q and U cubes
 
-    totalbad = None
+    Args:
+        qfile (str): Stokes Q fits file
+        ufile (str): Stokes U fits file
+        blank (bool, optional): Flag bad data when saved to disk. Defaults to False.
+        cliplev (float, optional): Number of stddev above median to flag. Defaults to 5.
+        iterate (int, optional): Number of flagging iterations. Defaults to 1.
+        ncores (int, optional): Number of cores to use. Defaults to 1.
+        outfile (str, optional): File to write flagged channels to. Defaults to None.
+    """
+    qcube = getcube(qfile)
+    ucube = getcube(ufile)
 
-    if clargs.mpi or clargs.n_cores == 1:
-        update = True
-    else:
-        update = False
+    assert len(ucube.spectral_axis) == len(qcube.spectral_axis), "Cubes have different number of channels"
 
-    totalbad = getbadchans(
-        pool,
-        qcube,
-        ucube,
-        clargs.qfitslist,
-        clargs.ufitslist,
-        totalbad=totalbad,
-        cliplev=clargs.cliplev,
-        update=update,
-    )
-    # print fitslist[np.asarray(badones,dtype=int)]
-
-    q_msk, u_msk = blankchans(qcube, ucube, totalbad, blank=clargs.blank)
-
-    if clargs.iterate is not None:
-        print(f"Iterating {clargs.iterate} additional time(s)...")
-        for i in range(clargs.iterate):
-            totalbad = getbadchans(
-                pool,
-                q_msk,
-                u_msk,
-                clargs.qfitslist,
-                clargs.ufitslist,
-                totalbad=totalbad,
-                cliplev=clargs.cliplev,
-                update=update,
-            )
-            q_msk, u_msk = blankchans(q_msk, u_msk, totalbad, blank=clargs.blank)
-
-    if clargs.blank:
-        writefits(q_msk, u_msk, clargs)
-
-    if clargs.file is not None:
-        print(f"Saving bad files to {clargs.file}")
-        np.savetxt(clargs.file, totalbad)
+    # Iterate
+    for i in range(iterate):
+        print(f"Flagging iteration {i+1} of {iterate}")
+        totalbad = getbadchans(
+            qcube,
+            ucube,
+            cliplev=cliplev,
+            ncores=ncores,
+        )
+        qcube, ucube = blankchans(qcube, ucube, totalbad, blank=blank)
 
 
-def cli():
+    if blank:
+        writefits(qcube, qcube, qfile, ufile)
+
+    if outfile is not None:
+        print(f"Saving bad files to {outfile}")
+        np.savetxt(outfile, totalbad)
+
+
+def cli() -> None:
     descStr = """
     Find bad channels by checking statistics of each channel image.
 
@@ -238,64 +167,60 @@ def cli():
     warnings.filterwarnings("ignore", message="overflow encountered in square")
 
     # Parse the command line options
-    ap = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description=descStr, formatter_class=argparse.RawTextHelpFormatter
     )
-    ap.add_argument("qfitslist", help="Wildcard list of Q fits files")
-    ap.add_argument("ufitslist", help="Wildcard list of U fits files")
-    ap.add_argument(
-        "--blank",
+    parser.add_argument("qfile", type=str, help="Stokes Q fits file")
+    parser.add_argument("ufile", type=str, help="Stokes U fits file")
+    parser.add_argument(
         "-b",
-        help="Blank bad channel maps? [default False]",
-        default=False,
+        "--blank",
+        help="Blank bad channels? [default False - just print out bad channels]",
         action="store_true",
     )
-    ap.add_argument(
-        "--cliplev",
+    parser.add_argument(
         "-c",
+        "--cliplev",
         help="Clip level in sigma, make this number lower to be more aggressive [default 5]",
         default=5.0,
         type=float,
     )
 
-    ap.add_argument(
-        "--iterate",
+    parser.add_argument(
         "-i",
-        help="Iterate flagging check N additional times [None -- one pass only]",
-        default=None,
+        "--iterate",
+        help="Iterate flagging check N times [dafult 1 -- one pass only]",
+        default=1,
         type=int,
     )
 
-    ap.add_argument(
-        "--file",
+    parser.add_argument(
         "-f",
+        "--file",
         help="Filename to write bad channel indices to file [None --  do not write]",
         default=None,
         type=str,
     )
 
-    group = ap.add_mutually_exclusive_group()
-
-    group.add_argument(
+    parser.add_argument(
         "--ncores",
         dest="n_cores",
         default=1,
         type=int,
-        help="Number of processes (uses multiprocessing).",
-    )
-    group.add_argument(
-        "--mpi", dest="mpi", default=False, action="store_true", help="Run with MPI."
+        help="Number of processes (uses joblib).",
     )
 
-    args = ap.parse_args()
+    args = parser.parse_args()
 
-    with schwimmbad.choose_pool(mpi=args.mpi, processes=args.n_cores) as pool:
-        if args.mpi:
-            if not pool.is_master():
-                pool.wait()
-                sys.exit(0)
-
-        main(pool, args)
+    main(
+        qfile=args.qfile,
+        ufile=args.ufile,
+        blank=args.blank,
+        cliplev=args.cliplev,
+        iterate=args.iterate,
+        ncores=args.n_cores,
+        outfile=args.file,
+    )
 
 
 if __name__ == "__main__":
