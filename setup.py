3ba59ef2103a0523da44557c5d3bb13bfd84e85d
Thomson, Alec (S&A, Kensington) <AlecThomson@users.noreply.github.com>
2022-12-08 15:12:51 +0800

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = "https://github.com/AlecThomson/RACS-tools"
 EMAIL = "alec.thomson@csiro.au"
 AUTHOR = "Alec Thomson"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "2.1.0"
+VERSION = "2.1.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
