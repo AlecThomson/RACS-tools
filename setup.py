fb672d4f8f0262bdcae97ce1de19a82d5b72d65c
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-07-20 13:10:39 +0800

    Fix reqs

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = "https://github.com/AlecThomson/RACS-tools"
 EMAIL = "alec.thomson@csiro.au"
 AUTHOR = "Alec Thomson"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "2.2.2"
+VERSION = "2.2.3"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -29,7 +29,6 @@ REQUIRED = [
     "schwimmbad",
     "psutil",
     "scipy",
-    "numpy",
     "spectral_cube",
     "tqdm",
 ]
