79c81b263cd24e083f738f358816024c3279cbca
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-07-26 13:16:32 +0800

    Bump version for visibility

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = "https://github.com/AlecThomson/RACS-tools"
 EMAIL = "alec.thomson@csiro.au"
 AUTHOR = "Alec Thomson"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "2.3.0"
+VERSION = "2.3.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -29,7 +29,7 @@ REQUIRED = [
     "schwimmbad",
     "psutil",
     "scipy",
-    "git+https://github.com/AlecThomson/spectral-cube@fix_beams", # Need this until PR is merged upstream :'( 
+    "spectral_cube @ git+https://github.com/AlecThomson/spectral-cube@fix_beams",  # Need this until PR is merged upstream :'(
     "tqdm",
 ]
 
