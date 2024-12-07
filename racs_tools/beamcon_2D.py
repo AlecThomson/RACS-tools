Fix for units and numpy ops (#73)
* Fix for units and numpy ops

* Fix for readonly array

* New test

racs_tools/beamcon_2D.py
@@ -324,7 +324,7 @@ def get_common_beam(
     flags = np.array([beam == ZERO_BEAM for beam in beams]) | flags
 
     if cutoff is not None:
-        flags = beams.major > cutoff * u.arcsec | flags
+        flags = beams.major.to(u.arcsec).value > cutoff | flags
         if np.all(flags):
             logger.critical(
                 "All beams are larger than cutoff. All outputs will be blanked!"

racs_tools/convolve_uv.py
@@ -536,7 +536,7 @@ def smooth(
     if np.any(np.isnan(new_image)):
         logger.warning(f"{np.isnan(new_image).sum()} NaNs present in smoothed output")
 
-    new_image *= fac
+    new_image = fac * new_image
 
     # Ensure the output data-type is the same as the input
     return new_image.astype(out_dtype)